63356cb81d08ac28e9b28db2e6f0041a8acb365f
Asaiy420 <kcaryan05@gmail.com>
2025-05-06 12:50:50 +0545

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+
 function App() {
   return (
-    <div className=""></div>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black p-6">
+      <div className="absolute inset-0 bg-[radial-gradient(#ffffff10_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+      <div className="relative">
+        <nav className="mb-6 flex gap-4">
+          <Link
+            to="/"
+            className="text-blue-400 hover:text-blue-300 transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            to="/signup"
+            className="text-blue-400 hover:text-blue-300 transition-colors"
+          >
+            Signup
+          </Link>
+          <Link
+            to="/login"
+            className="text-blue-400 hover:text-blue-300 transition-colors"
+          >
+            Login
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
