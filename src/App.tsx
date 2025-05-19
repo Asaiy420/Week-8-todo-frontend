16156a93e088f3420fa926894844ff2fd3e16544
Asaiy420 <kcaryan05@gmail.com>
2025-05-19 14:57:24 +0545

    fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,28 @@ function App() {
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black p-6">
       <div className="absolute inset-0 bg-[radial-gradient(#ffffff10_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
       <div className="relative">
-        <nav className="mb-6 flex gap-4">
+        <nav className="flex mb-6 justify-around  gap-2">
           <Link
             to="/"
-            className="text-blue-400 hover:text-blue-300 transition-colors"
+            className="text-blue-400 hover:text-blue-500 transition-colors"
           >
             Home
           </Link>
           <Link
             to="/todo"
-            className="text-blue-400 hover:text-blue-300 transition-colors"
+            className="text-blue-400 hover:text-blue-500 transition-colors"
           >
             Todo
           </Link>
           <Link
             to="/signup"
-            className="text-blue-400 hover:text-blue-300 transition-colors"
+            className="text-blue-400 hover:text-blue-500 transition-colors"
           >
             Signup
           </Link>
           <Link
             to="/login"
-            className="text-blue-400 hover:text-blue-300 transition-colors"
+            className="text-blue-400 hover:text-blue-500 transition-colors"
           >
             Login
           </Link>
